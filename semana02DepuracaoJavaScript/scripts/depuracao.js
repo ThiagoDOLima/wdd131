1fb7ee38dd2d632237bb1cd7dda2ac265fb49919
ThiagoDOLima <tlima1@byupathway.edu>
2026-09-04 12:26:12 -0300

    depuracao erros

--- semana02DepuracaoJavaScript/scripts/depuracao.js
+++ semana02DepuracaoJavaScript/scripts/depuracao.js
@@ -1,15 +1,15 @@
 const resultadoRaio = document.getElementById('raio');
-const resultadoArea = document.querySelector('area');
+const resultadoArea = document.querySelector('#area');
 
 let area = 0;
-const PI == 3.14159;
+const PI = 3.14159;
 
-const raio = 10;
+let raio = 10;
 area = PI * raio * raio;
-resultadoRaio = raio;
-resultadoArea = area;
+resultadoRaio.textContent = raio;
+resultadoArea.textContent = area;
 
 raio = 20;
 area = PI * raio * raio;
-resultadoRaio = raio;
-resultadoArea = area;
+resultadoRaio.innerHTML = raio;
+resultadoArea.innerHTML = area;
