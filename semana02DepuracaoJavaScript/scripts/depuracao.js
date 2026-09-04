8dde1aa532bb1833a08bfa2be9b40d52e0898449
ThiagoDOLima <tlima1@byupathway.edu>
2026-09-04 12:08:32 -0300

    depuracao

---
+++ semana02DepuracaoJavaScript/scripts/depuracao.js
@@ -0,0 +1,15 @@
+const resultadoRaio = document.getElementById('raio');
+const resultadoArea = document.querySelector('area');
+
+let area = 0;
+const PI == 3.14159;
+
+const raio = 10;
+area = PI * raio * raio;
+resultadoRaio = raio;
+resultadoArea = area;
+
+raio = 20;
+area = PI * raio * raio;
+resultadoRaio = raio;
+resultadoArea = area;
