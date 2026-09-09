e2e48e966f1fede14bea2b762908b8d02025fb5a
ThiagoDOLima <tlima1@byupathway.edu>
2026-09-09 16:33:17 -0300

    menu hamburquer

--- semana02AlbumImagens/scripts/templos.js
+++ semana02AlbumImagens/scripts/templos.js
@@ -3,9 +3,10 @@ const menuLinks = document.getElementById('menuLinks');
 
 menuToggle.addEventListener('click', () => {
     menuLinks.classList.toggle('active');
+    menuToggle.classList.toggle('open');
 });
 
-document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById('currentyear').textContent = new Date().getFullYear();
 
-document.getElementById("ultimaModificacao").textContent =
+document.getElementById('ultimaModificacao').textContent =
     `Última modificação: ${document.lastModified}`;
