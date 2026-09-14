3bb0c860ba623d81c42d1bbef5d580ce0c5ef908
ThiagoDOLima <tlima1@byupathway.edu>
2026-09-14 15:40:51 -0300

    s03

---
+++ scripts/local.js
@@ -0,0 +1,4 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+document.getElementById("ultimaModificacao").textContent =
+    `Última modificação: ${document.lastModified}`;
